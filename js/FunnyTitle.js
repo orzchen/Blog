3698595e422be3d254ab1fc5785b33125ff3e7d2
XXXZhy <34533099+XXXZhy@users.noreply.github.com>
2020-06-07 10:39:18 +0800

    Add files via upload

---
+++ js/FunnyTitle.js
@@ -0,0 +1,19 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+	if (location.href != "https://www.xxxzhy.com/"){
+		if (document.hidden) {
+			//$('[rel="icon"]').attr('href', "/funny.ico");
+			document.title = '/(ㄒoㄒ)/你弄丢了XXXZhy';
+			clearTimeout(titleTime);
+		}else {
+			//$('[rel="icon"]').attr('href', "/favicon.ico");
+			document.title = '(ฅ>ω<*ฅ) 噫又好啦~' + OriginTitle;
+			titleTime = setTimeout(function () {
+				document.title = OriginTitle;
+			}, 2000);
+		}
+	}
+    
+});
